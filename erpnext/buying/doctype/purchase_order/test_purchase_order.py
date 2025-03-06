a1644c7ebe828cc1733c1cb1a9150fd3d06f3a54
Bhavansathru <zeshanshakil0@gmail.com>
2025-03-06 17:51:20 +0530

    fix: validate accounting dimension company in Journal Entry & Stock Entry (#46204)
    * fix: validate accounting dimension company in journal entry and stock entry
    
    * test: update test cases to validate company-based accounting dimension
    
    * fix(test): ensure 'Pick List' company matches 'Delivery Note' to prevent test failures
    
    * chore: remove redundant lines of code

--- erpnext/buying/doctype/purchase_order/test_purchase_order.py
+++ erpnext/buying/doctype/purchase_order/test_purchase_order.py
@@ -941,6 +941,7 @@ class TestPurchaseOrder(IntegrationTestCase):
 		automatically_fetch_payment_terms(enable=0)
 
 	def test_internal_transfer_flow(self):
+		from erpnext.accounts.doctype.cost_center.test_cost_center import create_cost_center
 		from erpnext.accounts.doctype.sales_invoice.sales_invoice import (
 			make_inter_company_purchase_invoice,
 		)
@@ -956,8 +957,16 @@ class TestPurchaseOrder(IntegrationTestCase):
 		prepare_data_for_internal_transfer()
 		supplier = "_Test Internal Supplier 2"
 
+		create_cost_center(
+			cost_center_name="_Test Cost Center for perpetual inventory Account",
+			company="_Test Company with perpetual inventory",
+		)
+
 		mr = make_material_request(
-			qty=2, company="_Test Company with perpetual inventory", warehouse="Stores - TCP1"
+			qty=2,
+			company="_Test Company with perpetual inventory",
+			warehouse="Stores - TCP1",
+			cost_center="_Test Cost Center for perpetual inventory Account - TCP1",
 		)
 
 		po = create_purchase_order(
