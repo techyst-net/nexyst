9d9c060cd674a06dbc29b41492a013e07e153958
marination <zeshanshakil0@gmail.com>
2025-03-17 19:26:45 +0100

    fix: Headline rendered twice on first save
    - `refresh` gets triggered twice and that renders the note twice
    - Remove any existing note before rendering

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -2758,8 +2758,14 @@ erpnext.apply_putaway_rule = (frm, purpose=null) => {
 
 erpnext.set_unit_price_items_note = (frm) => {
 	if (frm.doc.has_unit_price_items && !frm.is_new()) {
-		frm.dashboard.set_headline_alert(
-			__("The {0} contains Unit Price Items with 0 Qty.", [__(frm.doc.doctype)]),
+		// Remove existing note
+		const $note = $(frm.layout.wrapper.find(".unit-price-items-note"));
+		if ($note.length) { $note.parent().remove(); }
+
+		frm.layout.show_message(
+			`<div class="unit-price-items-note">
+				${__("The {0} contains Unit Price Items.", [__(frm.doc.doctype)])}
+			</div>`,
 			"yellow",
 			true
 		);
