fd27a255f5baf3bfd29bb4062c219acb9a59bfbf
Prateek Karamchandani <zeshanshakil0@gmail.com>
2025-04-18 06:30:03 +0000

    feat: Ledger Preview for Stock Entry

--- erpnext/stock/doctype/stock_entry/stock_entry.js
+++ erpnext/stock/doctype/stock_entry/stock_entry.js
@@ -1081,6 +1081,8 @@ erpnext.stock.StockEntry = class StockEntry extends erpnext.stock.StockControlle
 		}
 		erpnext.hide_company(this.frm);
 		erpnext.utils.add_item(this.frm);
+		erpnext.accounts.ledger_preview.show_accounting_ledger_preview(this.frm);
+		erpnext.accounts.ledger_preview.show_stock_ledger_preview(this.frm);
 	}
 
 	serial_no(doc, cdt, cdn) {
