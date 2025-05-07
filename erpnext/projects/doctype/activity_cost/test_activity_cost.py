18b270c48980f101c77574b8b85415e39b063035
ruthra kumar <zeshanshakil0@gmail.com>
2025-05-07 12:19:30 +0530

    refactor(test): Activity Cost to use ERPNextTestSuite

--- erpnext/projects/doctype/activity_cost/test_activity_cost.py
+++ erpnext/projects/doctype/activity_cost/test_activity_cost.py
@@ -6,36 +6,15 @@ import frappe
 from frappe.tests import IntegrationTestCase
 
 from erpnext.projects.doctype.activity_cost.activity_cost import DuplicationError
+from erpnext.tests.utils import ERPNextTestSuite
 
 
-class TestActivityCost(IntegrationTestCase):
+class TestActivityCost(ERPNextTestSuite):
 	@classmethod
 	def setUpClass(cls):
 		super().setUpClass()
+		# TODO: only 1 employee is required
 		cls.make_employees()
-
-	@classmethod
-	def make_employees(cls):
-		records = [
-			{
-				"company": "_Test Company",
-				"date_of_birth": "1980-01-01",
-				"date_of_joining": "2010-01-01",
-				"department": "_Test Department - _TC",
-				"doctype": "Employee",
-				"first_name": "_Test Employee",
-				"gender": "Female",
-				"naming_series": "_T-Employee-",
-				"status": "Active",
-				"user_id": "test@example.com",
-			},
-		]
-		cls.employees = []
-		for x in records:
-			if not frappe.db.exists("Employee", {"first_name": x.get("first_name")}):
-				cls.employees.append(frappe.get_doc(x).insert())
-			else:
-				cls.employees.append(frappe.get_doc("Employee", {"first_name": x.get("first_name")}))
 
 	def test_duplication(self):
 		frappe.db.sql("delete from `tabActivity Cost`")
