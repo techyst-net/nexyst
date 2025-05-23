1444ee4cc3ccaa93dc849643b317f6ae192a85c9
ruthra kumar <zeshanshakil0@gmail.com>
2025-05-23 14:04:07 +0530

    refactor: fetch party name on selection

--- erpnext/crm/doctype/contract/contract.py
+++ erpnext/crm/doctype/contract/contract.py
@@ -60,9 +60,16 @@ class Contract(Document):
 		self.name = _(name)
 
 	def validate(self):
+		self.set_missing_values()
 		self.validate_dates()
 		self.update_contract_status()
 		self.update_fulfilment_status()
+
+	def set_missing_values(self):
+		if not self.party_full_name:
+			field = self.party_type.lower() + "_name"
+			if res := frappe.db.get_value(self.party_type, self.party_name, field):
+				self.party_full_name = res
 
 	def before_submit(self):
 		self.signed_by_company = frappe.session.user
