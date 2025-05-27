726db40c311d4cbfd73dcccc25a1de52fa1f377a
Diptanil Saha <zeshanshakil0@gmail.com>
2025-05-27 14:48:25 +0530

    fix: patch to set grand total to default mop if old column exists (#47731)
    * fix: patch to set grand total to default mop if old column exists
    
    * chore: patches.txt

--- erpnext/patches/v15_0/set_grand_total_to_default_mop.py
+++ erpnext/patches/v15_0/set_grand_total_to_default_mop.py
@@ -2,8 +2,9 @@ import frappe
 
 
 def execute():
-	POSProfile = frappe.qb.DocType("POS Profile")
+	if frappe.db.has_column("POS Profile", "disable_grand_total_to_default_mop"):
+		POSProfile = frappe.qb.DocType("POS Profile")
 
-	frappe.qb.update(POSProfile).set(POSProfile.set_grand_total_to_default_mop, 1).where(
-		POSProfile.disable_grand_total_to_default_mop == 0
-	).run()
+		frappe.qb.update(POSProfile).set(POSProfile.set_grand_total_to_default_mop, 1).where(
+			POSProfile.disable_grand_total_to_default_mop == 0
+		).run()
