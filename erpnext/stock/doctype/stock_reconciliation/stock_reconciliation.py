0050ad7a674ba3fe4bf2de0cf649a3c797452448
rohitwaghchaure <zeshanshakil0@gmail.com>
2025-06-05 17:35:32 +0530

    fix: stock reco qty with inventory dimension (#47918)

--- erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
+++ erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
@@ -884,6 +884,10 @@ class StockReconciliation(StockController):
 
 		self.update_inventory_dimensions(row, data)
 
+		if self.docstatus == 1 and has_dimensions and (not row.batch_no or not row.serial_and_batch_bundle):
+			data.qty_after_transaction = data.actual_qty
+			data.actual_qty = 0.0
+
 		return data
 
 	def make_sle_on_cancel(self):
