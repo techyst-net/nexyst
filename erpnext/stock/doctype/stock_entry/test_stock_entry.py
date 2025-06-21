2c58ef5232b5797c6722320b6d8a66a6799924e9
ruthra kumar <zeshanshakil0@gmail.com>
2025-06-21 09:12:09 +0530

    refactor(test): flaky stock entry; load records json

--- erpnext/stock/doctype/stock_entry/test_stock_entry.py
+++ erpnext/stock/doctype/stock_entry/test_stock_entry.py
@@ -56,6 +56,9 @@ def get_sle(**args):
 
 
 class TestStockEntry(ERPNextTestSuite):
+	def setUp(self):
+		self.load_test_records("Stock Entry")
+
 	def tearDown(self):
 		frappe.db.rollback()
 		frappe.set_user("Administrator")
