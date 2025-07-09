322a1284bbca029870371fa407922781364dca53
Patrick Eißler <zeshanshakil0@gmail.com>
2025-07-09 13:52:12 +0200

    feat(BOM): improve tree display with item_name and qty (#48176)
    Co-authored-by: Raffael Meyer <zeshanshakil0@gmail.com>

--- erpnext/manufacturing/doctype/bom/bom.py
+++ erpnext/manufacturing/doctype/bom/bom.py
@@ -1336,7 +1336,7 @@ def get_children(parent=None, is_root=False, **filters):
 
 		bom_items = frappe.get_all(
 			"BOM Item",
-			fields=["item_code", "bom_no as value", "stock_qty"],
+			fields=["item_code", "bom_no as value", "stock_qty", "qty"],
 			filters=[["parent", "=", frappe.form_dict.parent]],
 			order_by="idx",
 		)
