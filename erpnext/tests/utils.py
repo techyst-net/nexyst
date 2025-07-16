9e6e3d479257cee20fdb6cce4680ab4c95d93329
ruthra kumar <zeshanshakil0@gmail.com>
2025-07-16 14:34:12 +0530

    refactor(test): set_user utility method

--- erpnext/tests/utils.py
+++ erpnext/tests/utils.py
@@ -2490,6 +2490,15 @@ class ERPNextTestSuite(unittest.TestCase):
 					)
 				)
 
+	@contextmanager
+	def set_user(self, user: str):
+		try:
+			old_user = frappe.session.user
+			frappe.set_user(user)
+			yield
+		finally:
+			frappe.set_user(old_user)
+
 
 @ERPNextTestSuite.registerAs(staticmethod)
 @contextmanager
