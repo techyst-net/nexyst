d51d1b1b8e0535c2e3c4413ff02e6c67ca09abe9
Dev Dusija <zeshanshakil0@gmail.com>
2025-07-22 23:56:48 +0530

    fix: account currency validation to exclude cancelled entries

--- erpnext/accounts/doctype/account/account.py
+++ erpnext/accounts/doctype/account/account.py
@@ -302,7 +302,9 @@ class Account(NestedSet):
 			self.account_currency = frappe.get_cached_value("Company", self.company, "default_currency")
 			self.currency_explicitly_specified = False
 
-		gl_currency = frappe.db.get_value("GL Entry", {"account": self.name}, "account_currency")
+		gl_currency = frappe.db.get_value(
+			"GL Entry", {"account": self.name, "is_cancelled": 0}, "account_currency"
+		)
 
 		if gl_currency and self.account_currency != gl_currency:
 			if frappe.db.get_value("GL Entry", {"account": self.name}):
