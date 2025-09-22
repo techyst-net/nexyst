3375b1b09d32f5009746041fce39c214bc8eb3af
Kavin <zeshanshakil0@gmail.com>
2025-09-22 18:54:57 +0530

    fix: auto commit if too many writes reached

--- erpnext/patches/v15_0/set_company_on_pos_inv_merge_log.py
+++ erpnext/patches/v15_0/set_company_on_pos_inv_merge_log.py
@@ -6,6 +6,7 @@ def execute():
 		"POS Invoice Merge Log", {"docstatus": 1}, ["name", "pos_closing_entry"]
 	)
 
+	frappe.db.auto_commit_on_many_writes = 1
 	for log in pos_invoice_merge_logs:
 		if log.pos_closing_entry and frappe.db.exists("POS Closing Entry", log.pos_closing_entry):
 			company = frappe.db.get_value("POS Closing Entry", log.pos_closing_entry, "company")
