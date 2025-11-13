3341d0b1bafa58ca02ba30c5987a19500585b031
ruthra kumar <zeshanshakil0@gmail.com>
2025-11-13 13:27:03 +0530

    refactor(test): make maintenance schedule deterministic

--- erpnext/maintenance/doctype/maintenance_schedule/test_maintenance_schedule.py
+++ erpnext/maintenance/doctype/maintenance_schedule/test_maintenance_schedule.py
@@ -18,6 +18,7 @@ class TestMaintenanceSchedule(ERPNextTestSuite):
 	@classmethod
 	def setUpClass(cls):
 		super().setUpClass()
+		cls.load_test_records("Stock Entry")
 		cls.make_sales_person()
 
 	@classmethod
