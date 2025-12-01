e51aac3c2134c9ab801147d7d28ba91a43bef652
khushi8112 <zeshanshakil0@gmail.com>
2025-12-01 12:53:33 +0530

    fix: patch to migrate setting

--- erpnext/patches/v15_0/migrate_account_freezing_settings_to_company.py
+++ erpnext/patches/v15_0/migrate_account_freezing_settings_to_company.py
@@ -2,17 +2,18 @@ import frappe
 
 
 def execute():
-	accounts_settings = frappe.get_doc("Accounts Settings", "Accounts Settings")
+	frozen_till = frappe.db.get_single_value("Accounts Settings", "acc_frozen_upto")
+	modifier = frappe.db.get_single_value("Accounts Settings", "frozen_accounts_modifier")
 
-	accounts_frozen_till_date = accounts_settings.acc_frozen_upto
-	frozen_accounts_modifier = accounts_settings.frozen_accounts_modifier
+	if not frozen_till and not modifier:
+		return
 
 	for company in frappe.get_all("Company", pluck="name"):
 		frappe.db.set_value(
 			"Company",
 			company,
 			{
-				"accounts_frozen_till_date": accounts_frozen_till_date,
-				"role_allowed_for_frozen_entries": frozen_accounts_modifier,
+				"accounts_frozen_till_date": frozen_till,
+				"role_allowed_for_frozen_entries": modifier,
 			},
 		)
