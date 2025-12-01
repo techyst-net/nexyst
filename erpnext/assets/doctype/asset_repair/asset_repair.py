369786f3ffab0bc449aeb889cc4a77fd8596e880
ljain112 <zeshanshakil0@gmail.com>
2025-12-01 13:17:47 +0530

    refactor: linters

--- erpnext/assets/doctype/asset_repair/asset_repair.py
+++ erpnext/assets/doctype/asset_repair/asset_repair.py
@@ -553,7 +553,7 @@ def get_unallocated_repair_cost(
 	if not purchase_invoice or not expense_account:
 		return 0.0
 
-	frappe.has_permission("Purchase Invoice","read", purchase_invoice, throw=True)
+	frappe.has_permission("Purchase Invoice", "read", purchase_invoice, throw=True)
 
 	used_amount = get_allocated_repair_cost(purchase_invoice, expense_account, exclude_asset_repair)
 	total_amount = get_total_expense_amount(purchase_invoice, expense_account)
