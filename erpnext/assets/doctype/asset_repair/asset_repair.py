eddd7f58c2de2655710aa57791802ed41b2ff4a4
ljain112 <zeshanshakil0@gmail.com>
2025-12-01 17:06:42 +0530

    chore: remove unused import for depreciation schedule

--- erpnext/assets/doctype/asset_repair/asset_repair.py
+++ erpnext/assets/doctype/asset_repair/asset_repair.py
@@ -12,7 +12,6 @@ from erpnext.accounts.general_ledger import make_gl_entries
 from erpnext.assets.doctype.asset.asset import get_asset_account
 from erpnext.assets.doctype.asset_activity.asset_activity import add_asset_activity
 from erpnext.assets.doctype.asset_depreciation_schedule.asset_depreciation_schedule import (
-	get_depr_schedule,
 	reschedule_depreciation,
 )
 from erpnext.controllers.accounts_controller import AccountsController
