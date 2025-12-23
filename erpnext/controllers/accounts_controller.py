0f948cac58a0737e921c8fb58631dd5931750648
Lakshit Jain <zeshanshakil0@gmail.com>
2025-12-23 20:47:53 +0530

    feat: Introduce tax withholding entry

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -297,8 +297,6 @@ class AccountsController(TransactionBase):
 
 		if self.doctype == "Purchase Invoice":
 			self.calculate_paid_amount()
-			# apply tax withholding only if checked and applicable
-			self.set_tax_withholding()
 
 		with temporary_flag("company", self.company):
 			validate_regional(self)
@@ -1098,6 +1096,12 @@ class AccountsController(TransactionBase):
 						"is_fixed_asset"
 					):
 						item.set("is_fixed_asset", ret.get("is_fixed_asset", 0))
+
+					if self.doctype in ["Purchase Invoice", "Sales Invoice"] and item.meta.get_field(
+						"tax_withholding_category",
+					):
+						if not item.get("tax_withholding_category") and ret.get("tax_withholding_category"):
+							item.set("tax_withholding_category", ret.get("tax_withholding_category"))
 
 					# Double check for cost center
 					# Items add via promotional scheme may not have cost center set
