14c5b64a21671000a228b4d461622a4e0489c644
Mihir Kandoi <zeshanshakil0@gmail.com>
2026-01-12 14:17:58 +0530

    chore: make feature opt in

--- erpnext/accounts/utils.py
+++ erpnext/accounts/utils.py
@@ -1530,8 +1530,12 @@ def parse_naming_series_variable(doc, variable):
 	else:
 		data = {"YY": "%y", "YYYY": "%Y", "MM": "%m", "DD": "%d", "JJJ": "%j"}
 		date = (
-			getdate(doc.get("posting_date") or doc.get("transaction_date") or doc.get("posting_datetime"))
-			or now_datetime()
+			(
+				getdate(doc.get("posting_date") or doc.get("transaction_date") or doc.get("posting_datetime"))
+				or now_datetime()
+			)
+			if frappe.get_single_value("Global Defaults", "use_posting_datetime_for_naming_documents")
+			else now_datetime()
 		)
 		return date.strftime(data[variable]) if variable in data else determine_consecutive_week_number(date)
 
