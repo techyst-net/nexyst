4213b31d79b91d3a211f94efa4b6631560dc3ab5
SowmyaArunachalam <zeshanshakil0@gmail.com>
2026-01-27 15:37:14 +0530

    fix: handle parent level project change

--- erpnext/public/js/utils/sales_common.js
+++ erpnext/public/js/utils/sales_common.js
@@ -496,18 +496,28 @@ erpnext.sales_common = {
 			}
 
 			project(doc, cdt, cdn) {
-				const item = frappe.get_doc(cdt, cdn);
-				if (item.project) {
-					$.each(this.frm.doc["items"] || [], function (i, other_item) {
-						if (!other_item.project) {
-							frappe.model.set_value(
-								other_item.doctype,
-								other_item.name,
-								"project",
-								item.project
-							);
-						}
-					});
+				if (!cdt || !cdn) {
+					if (this.frm.doc.project) {
+						$.each(this.frm.doc["items"] || [], function (i, item) {
+							if (!item.project) {
+								frappe.model.set_value(item.doctype, item.name, "project", doc.project);
+							}
+						});
+					}
+				} else {
+					const item = frappe.get_doc(cdt, cdn);
+					if (item.project) {
+						$.each(this.frm.doc["items"] || [], function (i, other_item) {
+							if (!other_item.project) {
+								frappe.model.set_value(
+									other_item.doctype,
+									other_item.name,
+									"project",
+									item.project
+								);
+							}
+						});
+					}
 				}
 				let me = this;
 				if (["Delivery Note", "Sales Invoice", "Sales Order"].includes(this.frm.doc.doctype)) {
