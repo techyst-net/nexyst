497de7dedd55dd65ec2bd86e111a822943db839b
Mihir Kandoi <zeshanshakil0@gmail.com>
2026-02-04 12:02:14 +0530

    fix: return None instead of 0 if valuation rate is falsy

--- erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
+++ erpnext/stock/doctype/stock_reconciliation/stock_reconciliation.py
@@ -524,7 +524,7 @@ class StockReconciliation(StockController):
 
 			float_precision = frappe.db.get_default("float_precision") or 3
 			item_dict["rate"] = flt(item_dict.get("rate"), float_precision)
-			item.valuation_rate = flt(item.valuation_rate, float_precision)
+			item.valuation_rate = flt(item.valuation_rate, float_precision) if item.valuation_rate else None
 			if (
 				(item.qty is None or item.qty == item_dict.get("qty"))
 				and (item.valuation_rate is None or item.valuation_rate == item_dict.get("rate"))
