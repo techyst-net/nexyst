20cfca70d171b13750a7137caa35659132565c73
Mihir Kandoi <zeshanshakil0@gmail.com>
2026-02-19 04:45:20 +0530

    fix: add purchase invoice as well

--- erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
+++ erpnext/stock/doctype/serial_and_batch_bundle/serial_and_batch_bundle.py
@@ -1026,7 +1026,7 @@ class SerialandBatchBundle(Document):
 			qty_field = "consumed_qty"
 		elif row.get("doctype") == "Stock Entry Detail":
 			qty_field = "transfer_qty"
-		elif row.get("doctype") == "Sales Invoice Item":
+		elif row.get("doctype") in ["Sales Invoice Item", "Purchase Invoice Item"]:
 			qty_field = "stock_qty"
 
 		return qty_field
