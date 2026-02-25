0a2ce3b203023be01f8b89f849edf34bea1f42ce
Krishna Shirsath <zeshanshakil0@gmail.com>
2026-02-25 13:21:06 +0530

    feat(employee): Add birthdays and work anniversaries indicator in form ,list view enhancements and new empty state.

--- erpnext/setup/doctype/employee/employee_list.js
+++ erpnext/setup/doctype/employee/employee_list.js
@@ -1,11 +1,34 @@
 frappe.listview_settings["Employee"] = {
 	add_fields: ["status", "branch", "department", "designation", "image"],
 	filters: [["status", "=", "Active"]],
-	get_indicator: function (doc) {
+	get_indicator(doc) {
 		return [
 			__(doc.status, null, "Employee"),
 			{ Active: "green", Inactive: "red", Left: "gray", Suspended: "orange" }[doc.status],
 			"status,=," + doc.status,
 		];
+	},
+
+	onload(listview) {
+		listview.get_no_result_message = () => {
+			return `
+                <div class="msg-box no-border">
+                    <div class="mb-4">
+                        <svg class="icon icon-xl" style="stroke: var(--text-light);">
+                            <use href="#icon-small-file"></use>
+                        </svg>
+                    </div>
+                    <p>${__("No Active Employees Found. Prefer importing if you have many records.")}</p>
+                    <p>
+						<button class="btn btn-primary btn-sm btn-new-doc">
+							${__("Create New")}
+						</button>
+                        <button class="btn btn-default btn-sm" onclick="frappe.set_route('List', 'Data Import', {reference_doctype: 'Employee'})">
+                            ${__("Import Employees")}
+                        </button>
+                    </p>
+                </div>
+            `;
+		};
 	},
 };
