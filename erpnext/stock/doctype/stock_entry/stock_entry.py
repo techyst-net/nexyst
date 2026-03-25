4daabbac980b42ff7f5da37979a1adde7845fe20
Smit Vora <zeshanshakil0@gmail.com>
2026-03-25 10:43:28 +0530

    fix: create source_stock_entry to refer to original manufacturing entry

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -152,6 +152,7 @@ class StockEntry(StockController, SubcontractingInwardController):
 		select_print_heading: DF.Link | None
 		set_posting_time: DF.Check
 		source_address_display: DF.TextEditor | None
+		source_stock_entry: DF.Link | None
 		source_warehouse_address: DF.Link | None
 		stock_entry_type: DF.Link
 		subcontracting_inward_order: DF.Link | None
